862d2a9cead1cbafaa811e0ffa478f3b37f53728
shivamS21 <2019ucs0100@iitjammu.ac.in>
2024-10-07 00:50:54 +0530

    modify vercel.json

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -4,7 +4,7 @@ import { connectDB } from '@/lib/mongodb';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import redis from "@/lib/redis";
-let firstTime = true
+
 /** 
  * @desc Create Task
  * @route POST /api/task
@@ -62,8 +62,7 @@ export async function GET(req: Request) {
     let cachedData = await redis.get(`tasks:${userId}`) as string | null;
 
     let userTasks;
-    if (cachedData && !firstTime) {
-      firstTime = false
+    if (cachedData) {
       if (typeof cachedData === 'string') {
         try {
           userTasks = JSON.parse(cachedData)
@@ -76,7 +75,6 @@ export async function GET(req: Request) {
         userTasks = cachedData
       }
     } else {
-      firstTime = false
       userTasks = await Task.find({ userId });
       await redis.set(`tasks:${userId}`, JSON.stringify(userTasks)); 
     }
